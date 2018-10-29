3ca52200145e51ef967f63fa9cb89a3a84299b3e
Erik Fisher <efishr4@gmail.com>
2018-10-29 14:49:31 -0700

    initial project setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './client/components/App.jsx';
+import './client/styles/styles.scss';
+
+ReactDOM.render(<App />, document.getElementById('root'));
