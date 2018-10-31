315b83a92c20ead4a3d96008ceb4afe0112b2181
Paul Gi <paulsgi@PAUL-MAC.local>
2018-10-31 16:47:41 -0700

    established a few routes for router

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,28 +1,41 @@
 import React,{ Component } from 'react';
+import { connect } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MainContainer from './containers/MainContainer.jsx';
+import GameMenuContainer from './containers/GameMenuContainer.jsx';
 import Navigation from './components/Navigation.jsx';
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
 import FooterContainer from './containers/FooterContainer.jsx';
 
+const mapStateToProps = store => ({
+  appLocation: store.appLocation,
+  selectedGame: store.selectedGame
+});
+
+const mapDispatchToProps = dispatch => ({
+
+});
 class App extends Component {
 
   render() {
+    const { appLocation, selectedGame } = this.props;
+    
     return(
       <BrowserRouter>
         <div>
-          <Navigation />
+          <Navigation appLocation={appLocation} selectedGame={selectedGame}/>
           <Switch>
             <Route path='/' component={MainContainer} exact/>
+            <Route path='/gameMenu' component={GameMenuContainer} />
             <Route path='/login' component={Login}/>
             <Route path='/signup' component={Signup}/>
           </Switch>
-          <FooterContainer />
+          <FooterContainer appLocation={appLocation}/>
         </div>
       </BrowserRouter>
     );
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
