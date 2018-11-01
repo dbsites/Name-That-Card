a947f6350af54b6d9a6f30f274e3c12b19e9156b
Paul Gi <paulsgi@PAUL-MAC.local>
2018-11-01 16:41:42 -0700

    added render props to login and signup

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -6,22 +6,44 @@ import GameMenuContainer from './containers/GameMenuContainer.jsx';
 import Navigation from './components/Navigation.jsx';
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
-import FooterContainer from './containers/FooterContainer.jsx';
+import Footer from './components/Footer.jsx';
 import PrivacyPolicy from './components/PrivacyPolicy.jsx';
 import ToS from './components/ToS.jsx';
 import About from './components/About.jsx';
-
-
+import * as actions from './actions/actions';
 
 const mapStateToProps = store => ({
+  isLoggedIn: store.userReducer.isLoggedIn,
   selectedGame: store.gameListReducer.selectedGame,
+  signUpError: store.userReducer.signUpError,
 });
 
 const mapDispatchToProps = dispatch => ({
-
+  updateSignUpUsername: (event) => {
+    dispatch(actions.updateSignUpUsername(event));
+  },
+  updateSignUpPassword: (event) => {
+    dispatch(actions.updateSignUpPassword(event));
+  },
+  updateSignUpEmail: (event) => {
+    dispatch(actions.updateSignUpEmail(event));
+  },
+  submitSignUp: (obj) => {
+    dispatch(actions.submitSignUp(obj));
+  },
+  updateLoginUsername: (event) => {
+    dispatch(actions.updateLoginUsername(event));
+  },
+  updateLoginPassword: (event) => {
+    dispatch(actions.updateLoginPassword(event));
+  },
+  submitLogin: () => {
+    dispatch(actions.submitLogin());
+  },
 });
+
 const App = (props) => {
-  const { selectedGame } = props;
+  const { selectedGame, updateSignUpUsername, updateSignUpPassword, updateSignUpEmail, submitSignUp, updateLoginUsername, updateLoginPassword, submitLogin, isLoggedIn, signUpError } = props;
   console.log('selectedGame ', selectedGame);
 
   return (
@@ -31,13 +53,16 @@ const App = (props) => {
         <Switch>
           <Route path='/' component={MainContainer} exact />
           <Route path='/gameMenu' component={GameMenuContainer} />
-          <Route path='/login' component={Login} />
-          <Route path='/signup' component={Signup} />
+          <Route path='/login' render={(props) =>
+              <Login {...props} updateLoginUsername={updateLoginUsername} updateLoginPassword={updateLoginPassword} submitLogin={submitLogin} isLoggedIn={isLoggedIn} />} />
+          <Route path='/signup' 
+            render={(props) => <Signup {...props} signUpError={signUpError} updateSignUpUsername={updateSignUpUsername} updateSignUpPassword={updateSignUpPassword} updateSignUpEmail={updateSignUpEmail} submitSignUp={submitSignUp}/>}
+          />
           <Route path='/about' component={About} />
           <Route path='/terms-of-service' component={ToS} />
           <Route path='/privacy-policy' component={PrivacyPolicy} />
         </Switch>
-        <FooterContainer />
+        <Footer />
       </div>
     </BrowserRouter>
   );
