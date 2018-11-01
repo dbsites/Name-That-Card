d6ae0d3e2482a1fad2282dee846d48bdd85462b9
Erik Fisher <efishr4@gmail.com>
2018-10-31 22:41:24 -0700

    added dynamic rendering of footer content with NavLinks to render components

--- src/client/containers/FooterContainer.jsx
+++ src/client/containers/FooterContainer.jsx
@@ -1,23 +1,46 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Score from '../components/Score.jsx';
 
-// const mapStateToProps = store => ({
 
-// });
+const mapStateToProps = store => ({
+  score: store.gameReducer.score,
+  gameStarted: store.gameMenuReducer.gameStarted,
+});
 
-// const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = dispatch => ({
 
-// });
+});
 
-class FooterContainer extends Component {
-  render() {
-    return (
-      <div className="FooterContainer" >
-        <h1>Footer Container</h1>
-      </div>
-    );
+const FooterContainer = (props) => {
+  const { score, gameStarted } = props;
+
+  let about = <span className=""><NavLink to="/about">About</NavLink></span>;
+  let privacyPolicy = <span className=""><NavLink to="/privacy-policy">Privacy Policy</NavLink></span>;
+  let termsOfService = <span className=""><NavLink to="/terms-of-service">Terms of Service</NavLink></span>;
+  let contact = <span>Contact</span>;
+  let footerScore = '';
+  console.log('footer location ', window.location.pathname);
+
+  if (gameStarted) {
+    footerScore = <Score score={score} />;
+    about = '';
+    privacyPolicy = '';
+    termsOfService = '';
+    contact = '';
   }
-}
+
+  return (
+    <div className="FooterContainer">
+      <span>{about}</span>
+      <span>{privacyPolicy}</span>
+      <span>{termsOfService}</span>
+      <span>{contact}</span>
+      <div>{footerScore}</div>
+    </div>
+  );
+};
 
 
-export default connect(/*mapStateToProps, mapDispatchToProps*/)(FooterContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(FooterContainer);
