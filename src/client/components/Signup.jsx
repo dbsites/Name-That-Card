0dacb08a3116973bf5d7ff0a418074c1374db3ee
Paul Gi <paulsgi@PAUL-MAC.local>
2018-11-01 16:44:56 -0700

    added form and render props

--- src/client/components/Signup.jsx
+++ src/client/components/Signup.jsx
@@ -1,13 +1,27 @@
 import React, {Component} from 'react';
 
-class Signup extends Component {
-
-  render() {
-    return(
-      <h1>Signup</h1>
-    );
+const Signup = (props) =>  {
+  const { updateSignUpUsername, updateSignUpPassword, updateSignUpEmail, submitSignUp, signUpInputUsername, signUpInputPassword, signUpInputEmail } = props;
+  const signUpInfoObj = {
+    signUpInputUsername,
+    signUpInputEmail,
+    signUpInputPassword
   }
-
+  return (
+    <div className="SignUpContainer">
+      <h1>Sign Up</h1>
+      <p className="SignUpText">Username: </p>
+      <input id="username" className="SignUpInput" type="text" onChange={updateSignUpUsername}/>
+      <p className="SignUpText">Password: </p>
+      <input id="password" className="SignUpInput" type="password" onChange={updateSignUpPassword}/>
+      <p className="SignUpText">Email: </p>
+      <input id="email" className="SignUpInput" onChange={updateSignUpEmail}/>
+      <br />
+      <br />
+      <button className="SignUpButton" onClick={() => submitSignUp(signUpInfoObj)}>Sign up</button>
+    </div>
+  )
 }
+
 
 export default Signup;
