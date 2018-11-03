172eff36514c44be13abd9b3496fe82b0603ede2
Paul Gi <paulsgi@PAUL-MAC.local>
2018-11-02 17:28:23 -0700

    implementing game selection WIP

--- src/client/components/GameType.jsx
+++ src/client/components/GameType.jsx
@@ -1,15 +1,13 @@
 import React, {Component} from 'react';
 
-class GameType extends Component {
-
-  render() {
-    const { game } = this.props;
-    return(
-      <div>
-        {game}
-      </div>
-    );
-  }
+const GameType = (props) => {
+  const { game, setSelectedGame } = props;
+  return(
+    <div className="gameType" onClick={() => setSelectedGame(game)} >
+      {game}
+    </div>
+  );
 }
+
 
 export default GameType;
