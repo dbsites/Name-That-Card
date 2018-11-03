ee429732de8768cbcff0589b8bea030ceffb196c
Paul Gi <paulsgi@PAUL-MAC.local>
2018-11-02 17:30:47 -0700

    adding game list navigating WIP

--- src/client/containers/GameListContainer.jsx
+++ src/client/containers/GameListContainer.jsx
@@ -1,32 +1,50 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import GameType from '../components/GameType.jsx';
+import * as gameConfigActions from '../actions/gameConfigActions';
 
 const mapStateToProps = store => ({
   gameList: store.gameListReducer.gameList,
+  ableToProceed: store.gameListReducer.ableToProceed,
+  playClicked: store.gameListReducer.playClicked,
+  selectedGame: store.gameListReducer.selectedGame,
 });
 
 const mapDispatchToProps = dispatch => ({
-
+  setSelectedGame: (game) => {
+    dispatch(gameConfigActions.setSelectedGame(game));
+  },
+  submitGameSelection: (obj) => {
+    dispatch(gameConfigActions.submitGameSelection(obj));
+  },
 });
 
-class GameListContainer extends Component {
-  render() {
-    const { gameList } = this.props;
-    let gameLists = gameList.map((game, index) => {
-      return(
-        <GameType key={index} game={game} />
-      )
-    })
-    return (
-      <div className="GameListContainer" >
-        <h2>Game List Container</h2>
-        {gameLists}
-        <button>NEXT</button>
-      </div>
-    );
+const GameListContainer = (props) => {
+  const { gameList, setSelectedGame, submitGameSelection, ableToProceed, playClicked, selectedGame } = props;
+  const gameSelectedObj = {
+
+  };
+
+  let gameLists = gameList.map((game, index) => {
+    return(
+      <GameType key={index} game={game} setSelectedGame={setSelectedGame} />
+    )
+  })
+
+  if (ableToProceed && playClicked) {
+    return <Redirect to='/gameMenu' />;
   }
+
+  return (
+    <div className="GameListContainer" >
+      <h2>Game List Container</h2>
+      {gameLists}
+      <button onClick={() => submitGameSelection(selectedGame)} >PLAY</button>
+    </div>
+  );
 }
+
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(GameListContainer);
