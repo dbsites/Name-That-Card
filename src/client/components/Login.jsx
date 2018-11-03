615d63ee69b7f75edf4151ea7ab50c3f9e1cfcb2
Paul Gi <paulsgi@PAUL-MAC.local>
2018-11-02 17:28:43 -0700

    functioning login

--- src/client/components/Login.jsx
+++ src/client/components/Login.jsx
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import { NavLink } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 const Login = (props) => {
   const { updateLoginEmail, updateLoginPassword, submitLogin, isLoggedIn, loginInputEmail, loginInputPassword } = props;
 
   const loginInfoObj = {
-    loginInputEmail,
-    loginInputPassword,
+    email: loginInputEmail,
+    password: loginInputPassword,
   }
 
   if (isLoggedIn) {
