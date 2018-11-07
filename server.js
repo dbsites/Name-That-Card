90c7005f7a2a63c5a8b0238eebfc4b647cd365d1
Hari Kim <hari9497@Haris-MacBook-Pro.local>
2018-11-07 11:29:05 -0800

    added middlewares to routes

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const authController = require('./src/server/db/controllers/user/authController');
+const cookieController = require('./src/server/db/controllers/cookie/cookieController');
 const gameController = require('./src/server/db/controllers/game/gameController');
 const playController = require('./src/server/db/controllers/game/playController');
+const sessionController = require('./src/server/db/controllers/session/sessionController');
 
-
-const app = express(); 
-
+const app = express();
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
@@ -15,29 +16,34 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get('/rootpage',
+  sessionController.checkSSIDSession
+);
+
 app.post('/login',
   authController.verifyUser,
-  // cookieController.setSSIDCookie,
-  // set ssid
+  cookieController.setSSIDCookie,
+  sessionController.createSession,
   (req, res) => {
-    res.status(200).json({username: res.locals.verifiedUser.username, loginSuccess: true, msg: 'login success'});
+    res.status(200).json({ username: res.locals.verifiedUser.username, loginSuccess: true, msg: 'login success' });
   });
-
 
 app.post('/signup',
   authController.checkEmailExists,
   authController.checkUsernameExists,
   authController.createUser,
+  cookieController.setSSIDCookie,
+  sessionController.createSession,
   (req, res) => {
-    return res.status(200).json({signUpSuccess: true });
+    return res.status(200).json({ signUpSuccess: true });
   });
 
-  /**
-   * request object with 
-   * game name and level of difficulty
-   */
-  app.get('/gameList', gameController.gameList);
-  app.get('/gameMenu/:game', gameController.gameMenu);
-  app.post('/loadGame', playController.loadGame);
+/**
+ * request object with 
+ * game name and level of difficulty
+ */
+app.get('/gameList', gameController.gameList);
+app.get('/gameMenu/:game', gameController.gameMenu);
+app.post('/loadGame', playController.loadGame);
 
 app.listen(3000, () => console.log('server is listening on 3000'));
