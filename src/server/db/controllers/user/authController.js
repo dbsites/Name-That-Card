87eff9289c578357f6eadbfbf145cf3231a28350
Hari Kim <hari9497@Haris-MacBook-Pro.local>
2018-11-07 11:29:54 -0800

    added authorization logic

--- src/server/db/controllers/user/authController.js
+++ src/server/db/controllers/user/authController.js
@@ -5,7 +5,7 @@ const db = require('../util/postgres');
 
 module.exports = {
 
-  createUser(req, res, next) {
+  createUser: (req, res, next) => {
     const userInfo = req.body;
     const {
       username,
@@ -15,24 +15,23 @@ module.exports = {
     const userInputs = [username, password, email];
 
     addNewUser = () => {
-        db.none('INSERT INTO "game.dbo".users("username", "password", "email_address") VALUES($1, $2, $3)', userInputs)
-          .then(() => {
-           res.send({msg: `${username} created`});
-            console.log(`User ${username} created`) 
-          })
-          .catch(err => console.error(err));
-      },
-
+      db.none('INSERT INTO "game.dbo".users("username", "password", "email_address") VALUES($1, $2, $3)', userInputs)
+        .then(() => {
+          res.send({ msg: `${username} created` });
+          console.log(`User ${username} created`)
+        })
+        .catch(err => console.error(err));
+    },
       bcrypt.genSalt(SALT_WORK_FACTOR)
-      .then(salt => bcrypt.hash(password, salt))
-      .then((hash) => {
-        userInputs[1] = hash;
-      })
-      .then(() => addNewUser())
-      .catch(err => console.error(err));
+        .then(salt => bcrypt.hash(password, salt))
+        .then((hash) => {
+          userInputs[1] = hash;
+        })
+        .then(() => addNewUser())
+        .catch(err => console.error(err));
   },
 
-  checkEmailExists(req, res, next) {
+  checkEmailExists: (req, res, next) => {
     const { email } = req.body;
     db.any('SELECT * FROM "game.dbo".users where email_address=$1', [email])
       .then((data) => {
@@ -40,14 +39,13 @@ module.exports = {
           return res.send({
             msg: 'email already exists',
             signUpSuccess: false
-
           });
         } else return next();
       })
       .catch(err => console.error(err));
   },
 
-  checkUsernameExists(req, res, next) {
+  checkUsernameExists: (req, res, next) => {
     const { username } = req.body;
     db.any('SELECT * FROM "game.dbo".users where username=$1', [username])
       .then((data) => {
@@ -61,15 +59,16 @@ module.exports = {
       .catch(err => console.error(err));
   },
   verifyUser(req, res, next) {
-    const { email, password } = req.body; 
-    db.any('SELECT * FROM "game.dbo".users WHERE email_address=$1', [email])
+    const { email_address, password } = req.body;
+    db.any('SELECT * FROM "game.dbo".users WHERE email_address=$1', [email_address])
       .then((data) => {
         console.log('data', data)
         const user = data[0];
+        console.log('user****', user, '******');
         bcrypt.compare(password, user.password, (error, resolve) => {
           if (resolve) {
-            const { username, email } = user;
-            res.locals.verifiedUser = Object.assign(user)
+            res.locals.verifiedUser = user;
+            console.log('************* verified user', res.locals.verifiedUser);
             return next();
           }
           return res.status(400).send({
@@ -80,6 +79,4 @@ module.exports = {
       })
       .catch(err => console.error(err));
   },
-
-
 };
