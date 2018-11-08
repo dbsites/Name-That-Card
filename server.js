8044a9ff44e007e48e67308eb8f5530e3f623b2c
sratemo <sratemo@gmail.com>
2018-11-08 11:51:00 -0800

    multiple middleware added

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const authController = require('./src/server/db/controllers/user/authController');
 const gameController = require('./src/server/db/controllers/game/gameController');
 const playController = require('./src/server/db/controllers/game/playController');
+const playing = require('./src/server/db/controllers/game/playing');
+ 
+const finalPlay = require('./src/server/db/controllers/game/finalPlayController');
 
 
 const app = express();
@@ -34,10 +37,11 @@ app.post('/signup',
    */
 app.get('/gameList', gameController.gameList);
 app.get('/gameMenu/:game', gameController.gameMenu);
-app.post('/loadGame', playController.loadGame);
+// app.post('/loadGame', playController.loadGame);
 
 app.post('/wrongAnswers', playController.wrongAnswers);
-
 app.post('/saveScore', playController.saveScore);
+app.post('/loadGame', finalPlay.loadGame);
+ 
 
 app.listen(3000, () => console.log('server is listening on 3000'));
---
+++ src/server/db/controllers/game/finalPlayController.js
@@ -0,0 +1,90 @@
+const db = require('../util/postgres');
+
+/**
+ * but for other games like sports, a card will only appear once in its category
+ */
+module.exports = {
+
+  loadGame(req, res, next) {
+    console.log('body', req.body);
+    const {
+      game,
+      query,
+      difficulty_level,
+    } = req.body;
+    db.many(`
+          SELECT c.*
+            FROM "game.dbo".cards_n c
+              JOIN "game.dbo".game g 
+                ON g.game_id = c.game_id
+              WHERE g.game_name = $1 AND ($2:raw)  
+              ORDER BY RANDOM()
+              LIMIT 20;`, [game, query])
+      .then((cards) => {
+        res.locals.cards = cards;
+        return res.send(cards);
+      })
+      .catch(err => console.error(err));
+  },
+
+  EasyAnswers(req, res, next) {
+    const {
+      card_id,
+    } = req.body;
+    db.many(`        
+            SELECT c1.card_id, c1.card_name, 'EASY' as answer
+            FROM "game.dbo".cards_n c
+              JOIN "game.dbo".cards_n c1
+                ON c.card_category = c1.card_category 
+                AND c1.card_id <> $1
+            WHERE c.card_id = $1
+            ORDER BY RANDOM()
+            LIMIT 3;
+            `, [card_id])
+      .then((cards) => {
+        return res.send(cards);
+      })
+      .catch(err => console.error(err));
+  },
+
+  MediumAnswers(req, res, next) {
+    const { card_id } = req.body;
+    db.many(`SELECT c1.card_id, c1.card_name, 'MEDIUM' as answer
+              FROM "game.dbo".cards_n c
+              JOIN "game.dbo".cards_n c1
+                ON c.card_category = c1.card_category 
+                AND c.category_a = c1.category_a 
+                AND c1.card_id <> $1
+              WHERE c.card_id = $1
+              ORDER BY RANDOM()
+              LIMIT 3;
+            `, [card_id])
+      .then((cards) => {
+        return res.send(cards);
+      })
+      .catch(err => console.error(err));
+  },
+
+
+  hardAnswers(req, res, next) {
+    const { card_id, } = req.body;
+
+    db.many(`SELECT c1.card_id, c1.card_name, 'HARD' as answer
+              FROM "game.dbo".cards_n c
+              JOIN "game.dbo".cards_n c1
+                ON c.card_category = c1.card_category 
+                  AND c.category_a = c1.category_a 
+                  AND c.category_b = c1.category_b
+                  AND c1.card_id <> $1
+              WHERE c.card_id = $1
+              ORDER BY RANDOM()
+              LIMIT 3;
+            `, [card_id])
+      .then((cards) => {
+        return res.send(cards);
+      })
+      .catch(err => console.error(err));
+  },
+
+
+};
