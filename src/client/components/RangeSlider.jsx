fe179b731860d8683cb605cba4db6bc26e576823
Paul Gi <paulsgi@PAUL-MAC.local>
2018-11-12 16:13:27 -0800

    temp styling commit

--- src/client/components/RangeSlider.jsx
+++ src/client/components/RangeSlider.jsx
@@ -1,28 +1,13 @@
 import React from 'react';
-import Tooltip from 'rc-tooltip';
+// import Tooltip from 'rc-tooltip';
 import Slider from 'rc-slider';
 
-import 'rc-slider/assets/index.css';
-import 'rc-tooltip/assets/bootstrap.css';
+import '../styles/slider.scss';
+import '../styles/bootstrap.scss';
+import '../styles/bootstrap_white.scss';
 
 const createSliderWithTooltip = Slider.createSliderWithTooltip;
 const Range = createSliderWithTooltip(Slider.Range);
-const Handle = Slider.Handle;
-
-const handle = (props) => {
-  const { value, dragging, index, ...restProps } = props;
-  return (
-    <Tooltip
-      prefixCls="rc-slider-tooltip"
-      overlay={value}
-      visible={dragging}
-      placement="top"
-      key={index}
-    >
-      <Handle value={value} {...restProps} />
-    </Tooltip>
-  );
-};
 
 const RangeSlider = (props) => {
   return (
