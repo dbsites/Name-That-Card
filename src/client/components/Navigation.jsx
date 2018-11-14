e15cc6d376e03d5a1089cae45c18cdf62d96aaae
Erik Fisher <efishr4@gmail.com>
2018-11-14 10:19:57 -0800

    correct logo renders on applicable pages

--- src/client/components/Navigation.jsx
+++ src/client/components/Navigation.jsx
@@ -19,25 +19,25 @@ const Navigation = (props) => {
   
   let xBtn;
   
-  let logoUrl = 'https://s3-us-west-1.amazonaws.com/namethatcard/Logos+%2B+Icons/' + selectedGame + 'Logo.png';
+  let logoUrl = 'https://s3-us-west-1.amazonaws.com/namethatcard/Logos+%2B+Icons/GENERALIcon.png';
   
-  let logo = <div className="logo"> <img className="gameLogo" src={logoUrl}></img> </div>;  
+  let logo;  
 
   if (window.location.pathname === '/') {
     homeBtn = '';
     xBtn = '';
-    logoUrl = 'https://s3-us-west-1.amazonaws.com/namethatcard/Logos+%2B+Icons/GENERALIcon.png';
+    logoUrl = 'https://s3-us-west-1.amazonaws.com/namethatcard/Logos+%2B+Icons/GENERALIconWhite.png';
     document.body.style.backgroundImage = "url('https://s3-us-west-1.amazonaws.com/namethatcard/Background/GENERALBackground.jpg')";
   } else if (window.location.pathname === '/gameMenu/' + selectedGame) {
-    logo;
     xBtn = '';
+    logoUrl = 'https://s3-us-west-1.amazonaws.com/namethatcard/Logos+%2B+Icons/' + selectedGame + 'Logo.png';
     document.body.style.backgroundImage = "url('https://s3-us-west-1.amazonaws.com/namethatcard/Background/" + selectedGame + "Background.jpg')";
   } else if (window.location.pathname === '/game') {
-    logo;
     logInOutButton = '';
     homeBtn = '';
     signUpButton = '';
     navUsername = '';
+    logoUrl = 'https://s3-us-west-1.amazonaws.com/namethatcard/Logos+%2B+Icons/' + selectedGame + 'Logo.png';
     document.body.style.backgroundImage = "url('https://s3-us-west-1.amazonaws.com/namethatcard/Background/" + selectedGame + "Background.jpg')";
     const selectedGameRoute = `/gameMenu/${selectedGame}`
     xBtn = <div className="menu-item"><NavLink to={selectedGameRoute}>X</NavLink></div>;
@@ -47,17 +47,19 @@ const Navigation = (props) => {
   } else if (window.location.pathname === '/login') {
     logInOutButton = '';
     xBtn = '';
-    logo = <div className="logo"> NTC </div>
+    logoUrl = 'https://s3-us-west-1.amazonaws.com/namethatcard/Logos+%2B+Icons/GENERALIconWhite.png';
   } else if (window.location.pathname === '/signup') {
     signUpButton = '';
     xBtn = '';
-    logo = <div className="logo"> NTC </div>
+    logoUrl = 'https://s3-us-west-1.amazonaws.com/namethatcard/Logos+%2B+Icons/GENERALIconWhite.png';
   } else if (window.location.pathname === '/admin') {
     signUpButton = '';
     xBtn = '';
     logInOutButton = '';
     logo = '';
   }
+  
+  logo = <div className="logo"> <img className="gameLogo" src={logoUrl}></img> </div>; 
 
   return (
     <div className="navigation">
