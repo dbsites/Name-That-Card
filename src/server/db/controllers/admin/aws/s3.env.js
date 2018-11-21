9b9a40cc5f9a2e9358b8c99462f1d9b611945f66
Samuel Ratemo <sratemo@users.noreply.github.com>
2018-11-20 19:26:27 -0800

    Update s3.env.js

--- src/server/db/controllers/admin/aws/s3.env.js
+++ src/server/db/controllers/admin/aws/s3.env.js
@@ -1,6 +1,6 @@
 const env = {
-  AWS_ACCESS_KEY: 'AKIAJMAU5DW6VEZPUWAQ',
-  AWS_SECRET_ACCESS_KEY: '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',
+  AWS_ACCESS_KEY: '',
+  AWS_SECRET_ACCESS_KEY: '',
   REGION: 'us-east-2',
   Bucket: 'namethatcard',
 };
