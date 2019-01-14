7dea7ba20140057b3905b37fb791a46f3f24f149
smozingo <schno.mozingo@gmail.com>
2019-01-14 13:36:28 -0800

    updated s3 bucket to dbsites s3

--- src/server/db/controllers/admin/aws/s3_listObjets.js
+++ src/server/db/controllers/admin/aws/s3_listObjets.js
@@ -5,7 +5,7 @@ const AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json');
 // Set the region
 AWS.config.update({
-  region: 'us-west-1',
+  region: 'us-west-2',
 });
 
 
@@ -14,7 +14,7 @@ const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 
 // Create the parameters for calling createBucket
 const bucketParams = {
-  Bucket: 'namethatcard',
+  Bucket: 'elasticbeanstalk-us-west-2-295078398723',
 };
 // Call S3 to create the bucket
 s3.listObjects(bucketParams, (err, data) => {
