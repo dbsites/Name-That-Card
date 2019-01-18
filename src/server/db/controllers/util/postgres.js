4dad7d1780f511c55e989cc6afe97889ec3dfbf3
smozingo <schno.mozingo@gmail.com>
2019-01-18 15:41:41 -0800

    revised to work with env vars in prod

--- src/server/db/controllers/util/postgres.js
+++ src/server/db/controllers/util/postgres.js
@@ -1,6 +1,17 @@
 
 const pgp = require('pg-promise')({});
 
+if(process.env.NODE_ENV === 'production') {
+  const cn = {
+    host: process.env.RDS_HOSTNAME,
+    port: process.env.RDS_PORT,
+    database: process.env.RDS_DB_NAME,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+
+  };
+} else {
+
 const cn = {
   host: 'namethatcard-dev.cgbcdoczmmnf.us-east-1.rds.amazonaws.com',
   port: 5432,
@@ -8,7 +19,9 @@ const cn = {
   user: 'root',
   password: '12345678',
 
-};
+ };
+}
+console.log(Date(), `Connecting to ${cn.database} on ${cn.host}`);
 const db = pgp(cn);
 
 module.exports = db;
