af5b6b7379283778a372b77feeebaf7039bfd59a
smozingo <schno.mozingo@gmail.com>
2019-01-18 17:07:37 -0800

    debugging and changed user

--- src/server/db/controllers/util/postgres.js
+++ src/server/db/controllers/util/postgres.js
@@ -1,8 +1,11 @@
 
 const pgp = require('pg-promise')({});
 
+let cn = {}
+
 if(process.env.NODE_ENV === 'production') {
-  const cn = {
+  console.log('Running in production mode');
+  cn = {
     host: process.env.RDS_HOSTNAME,
     port: process.env.RDS_PORT,
     database: process.env.RDS_DB_NAME,
@@ -11,14 +14,13 @@ if(process.env.NODE_ENV === 'production') {
 
   };
 } else {
-
-const cn = {
-  host: 'namethatcard-dev.cgbcdoczmmnf.us-east-1.rds.amazonaws.com',
-  port: 5432,
-  database: 'namecard',
-  user: 'root',
-  password: '12345678',
-
+  console.log('Running in development mode');
+  cn = {
+    host: 'namethatcard-dev.cgbcdoczmmnf.us-east-1.rds.amazonaws.com',
+    port: 5432,
+    database: 'namecard',
+    user: 'root',
+    password: '12345678',
  };
 }
 console.log(Date(), `Connecting to ${cn.database} on ${cn.host}`);
