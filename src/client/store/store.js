c8035d1ee570b333ddb94db98c048228a8d897d0
Paul Gi <paulsgi@PAUL-MAC.local>
2019-01-28 18:10:29 -0800

    code clean up

--- src/client/store/store.js
+++ src/client/store/store.js
@@ -5,26 +5,26 @@ import reducers from '../reducers/combineReducers';
 
 function saveToLocalStorage(state) {
   try {
-    const serializedState = JSON.stringify(state)
-    localStorage.setItem('state', serializedState)
-  } catch(e) {
-    console.log(e)
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (e) {
+    console.log(e);
   }
 }
 
 function loadFromLocalStorage() {
   try {
-    const serializedState = localStorage.getItem('state')
-    if (serializedState === null) return undefined
-    return JSON.parse(serializedState)
-  } catch(e) {
-    console.log(e)
-    return undefined
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (e) {
+    console.log(e);
+    return undefined;
   }
 }
 
 const persistedState = loadFromLocalStorage();
- 
+
 const store = createStore(
   reducers,
   persistedState,
