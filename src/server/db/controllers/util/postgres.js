2b21b392a25ae3015ab6ede831482424dd95bbdb
smozingo <schno.mozingo@gmail.com>
2019-02-27 11:45:25 -0800

    set up to work with postgres container in dev

--- src/server/db/controllers/util/postgres.js
+++ src/server/db/controllers/util/postgres.js
@@ -6,22 +6,29 @@ let cn = {}
 if(process.env.NODE_ENV === 'production') {
   console.log('Running in production mode');
   cn = {
-    host: process.env.RDS_HOSTNAME,
-    port: process.env.RDS_PORT,
-    database: process.env.RDS_DB_NAME,
-    user: process.env.RDS_USERNAME,
+    host: process.env.RDS_HOSTNAME,     
+    port: process.env.RDS_PORT,         
+    database: process.env.RDS_DB_NAME,  
+    user: process.env.RDS_USERNAME,     
     password: process.env.RDS_PASSWORD,
 
   };
 } else {
   console.log('Running in development mode');
   cn = {
-    host: 'namethatcard-dev.cgbcdoczmmnf.us-east-1.rds.amazonaws.com',
+    host: 'postgres-db',
     port: 5432,
-    database: 'namecard',
-    user: 'root',
-    password: '12345678',
- };
+    database: 'namethatcard',
+    user: 'dbates',
+    password: 'admin',
+  };
+
+  //   host: 'namethatcard-dev.cgbcdoczmmnf.us-east-1.rds.amazonaws.com',
+  //   port: 5432,
+  //   database: 'namecard',
+  //   user: 'root',
+  //   password: '12345678',
+
 }
 console.log(Date(), `Connecting to ${cn.database} on ${cn.host}`);
 const db = pgp(cn);
